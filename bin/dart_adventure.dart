97535fd4c41b27f1d63d87962a8d68a52772b286
Kevin Chang <38297595+kevinlichang@users.noreply.github.com>
2021-06-29 23:52:22 -0400

    boiler plate commit

---
+++ bin/dart_adventure.dart
@@ -0,0 +1,5 @@
+import 'package:dart_adventure/dart_adventure.dart' as dart_adventure;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_adventure.calculate()}!');
+}
---
+++ test/dart_adventure_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_adventure/dart_adventure.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
