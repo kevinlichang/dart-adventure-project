cdeab031e67aded86039ae6f6baa8485e2db7f22
changkev <38297595+kevinlichang@users.noreply.github.com>
2021-07-06 00:44:27 -0400

    implemented read Line sync

--- bin/dart_adventure.dart
+++ bin/dart_adventure.dart
@@ -1,19 +1,22 @@
-import 'dart:developer';
-
-import 'package:dart_adventure/dart_adventure.dart' as dart_adventure;
 import 'dart:io';
+import 'package:dart_adventure/dart_adventure.dart' as dart_adventure;
+
 
 void main(List<String> arguments) {
   print('Hello! Welcome to the Solar System!\n'
       'Enter your name:');
 
+  final jsonFile = File('planetarySystem.json').readAsLinesSync();
+
   final name = stdin.readLineSync();
 
-  print('Hello $name.\n'
-      'Let\'s go on an adventure\n'
-      'Do you want to go to a random planet?');
+  print(
+    'Hello $name.\n'
+    'Let\'s go on an adventure\n'
+    'Do you want to go to a random planet?'
+  );
 
-  var user_input;
+  String user_input;
 
   while (user_input != 'Y' && user_input != 'N') {
     user_input = stdin.readLineSync();
@@ -21,7 +24,12 @@ void main(List<String> arguments) {
     if (user_input == 'Y') {
       print('yes');
     } else if (user_input == 'N') {
-      print('No');
+      print('Enter the planet you would like to visit.');
+      final planetName = stdin.readLineSync();
+      print(
+        'Traveling to $planetName\n'
+        'Arrived at $planetName. Yay'
+      );
     } else {
       print('Sorry, that answer is invalid. Try again!');
     }
